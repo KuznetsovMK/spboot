abd0b6a2b8494d5a141a9934e8e066d6b53b1c1e
KuznetsovM <kuznetcoff@mail.ru>
2021-12-13 10:57:41 +0300

    init commit

---
+++ src/main/java/ru/gb/sbboot/SbBootApplication.java
@@ -0,0 +1,14 @@
+package ru.gb.sbboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbBootApplication.class, args);
+	}
+
+}
